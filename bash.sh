a2fd208871141be7df36759bbf0e6ed3be5d3bfb
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-04-24 16:50:44 +0200

    ajout du fichier bash contenant les CLI permettant d'ouvrir le dissier contenant les sauvegarde de listes json des users folder

---
+++ bash.sh
@@ -0,0 +1,17 @@
+# Pour ouvrir le dossier contenant les enregistrementss de listes Json nicotine+ :
+
+# macOS
+
+open .local/share/nicotine/usershares/
+
+
+# linux 
+
+xdg-open ~/.local/share/nicotine/usershares/
+
+
+# Windows
+
+# a rectifier car le dossier .nicotine n'existe pas sous windows
+start %USERPROFILE%\.local\share\nicotine\usershares\ 
+
